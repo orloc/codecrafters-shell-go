3aa37cab3482d5078b19c269f83254513d1db232
Grant Tepper <teppga52@gmail.com>
2026-02-21 13:43:37 -0800

    updates git status

--- app/main.go
+++ app/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print("$ ")
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// strip the \n
+	fmt.Printf("%s: command not found\n", strings.Replace(input, "\n", "", -1))
+
 }
